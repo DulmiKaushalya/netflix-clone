f037702631d405638fb5e022a8898cafd2bd82fc
DulmiKaushalya <146383047+DulmiKaushalya@users.noreply.github.com>
2025-01-18 09:37:31 +0530

    file creates

---
+++ src/components/TitileCards/TitileCards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TitileCards.css'
+
+const TitileCards = () => {
+  return (
+    <div className='titilecards'>
+      
+    </div>
+  )
+}
+
+export default TitileCards
